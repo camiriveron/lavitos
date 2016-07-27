772cc0a5538cc339f9fb8c5e9566a16200e1b37e
Camila Riveron <camila@isl.co>
2016-07-27 13:56:50 -0400

    header css

--- wp-content/themes/sela-wpcom/header.php
+++ wp-content/themes/sela-wpcom/header.php
@@ -19,8 +19,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<header id="masthead" class="site-header" role="banner">
+<header id="masthead" class="site-header" role="banner">
 		<a class="skip-link screen-reader-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'sela' ); ?>"><?php _e( 'Skip to content', 'sela' ); ?></a>
 
 		<div class="site-branding">
@@ -39,5 +38,5 @@
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
-
+<div id="page" class="hfeed site">
 	<div id="content" class="site-content">
