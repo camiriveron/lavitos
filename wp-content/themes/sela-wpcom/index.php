d11b22cad5b1318d84df7bd87e6010f7e637c1dc
Camila Riveron <camila@isl.co>
2016-07-27 21:52:13 -0400

    custom posts

--- wp-content/themes/sela-wpcom/index.php
+++ wp-content/themes/sela-wpcom/index.php
@@ -7,6 +7,23 @@
 
 get_header(); ?>
 
+	<?php if ( have_posts() ) : ?>
+
+		<h2>Recent Posts</h2>
+		<ul>
+		<?php
+			$args = array( 'numberposts' => '5',
+			 'orderby' => 'post_date',
+    		 'order' => 'DESC', );
+			$recent_posts = wp_get_recent_posts( $args );
+			foreach( $recent_posts as $recent ){
+				echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </li> ';
+			}
+		?>
+		</ul>
+
+	<?php endif; ?>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
