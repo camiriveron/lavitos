3d00d716c530a0513371f747837a1899f52fed9a
Camila Riveron <camila@isl.co>
2016-08-03 15:58:15 -0400

    carousel

--- wp-content/themes/sela-wpcom/index.php
+++ wp-content/themes/sela-wpcom/index.php
@@ -41,7 +41,7 @@ get_header(); ?>
         itemsDesktop : [1500,3], //5 items between 1000px and 901px
 		itemsDesktopSmall : [900,3], // betweem 900px and 601px
 		itemsTablet: [600,2], //2 items between 600 and 0
-		itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+		itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
         navigation : false,
         pagination: false,
         slideSpeed: 700,
