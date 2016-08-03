d76b67d9f2f96eee7b28a2d5bbb3e9bcf94ec15c
Camila Riveron <camila@isl.co>
2016-08-03 17:34:37 -0400

    thumbnail

--- wp-content/themes/sela-wpcom/functions.php
+++ wp-content/themes/sela-wpcom/functions.php
@@ -52,17 +52,17 @@ function sela_setup() {
 	add_theme_support( 'post-thumbnails' );
 
 	// Post thumbnails
-	set_post_thumbnail_size( 820, 312, true );
+	set_post_thumbnail_size( 820, 312, array( 'center', 'center' ) );
 	// Hero Image on the front page template
-	add_image_size( 'sela-hero-thumbnail', 1180, 610, true );
+	add_image_size( 'sela-hero-thumbnail', 1180, 610, array( 'center', 'center' ) );
 	// Full width and grid page template
-	add_image_size( 'sela-page-thumbnail', 1180, 435, true );
+	add_image_size( 'sela-page-thumbnail', 1180, 435, array( 'center', 'center' ) );
 	// Full width and grid page template
 	add_image_size( 'sela-post-thumbnail', 820, 312, array( 'center', 'center' ) );
 	// Grid child page thumbnail
-	add_image_size( 'sela-grid-thumbnail', 360, 242, true );
+	add_image_size( 'sela-grid-thumbnail', 360, 242, array( 'center', 'center' ) );
 	// Testimonial thumbnail
-	add_image_size( 'sela-testimonial-thumbnail', 90, 90, true );
+	add_image_size( 'sela-testimonial-thumbnail', 90, 90, array( 'center', 'center' ) );
 
 	add_post_type_support( 'page', 'excerpt' );
 
