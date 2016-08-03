204eb10155676153c7842287f78583cdf2d0ecdf
Camila Riveron <camila@isl.co>
2016-08-03 12:24:34 -0400

    carousel

--- wp-content/themes/sela-wpcom/header.php
+++ wp-content/themes/sela-wpcom/header.php
@@ -53,8 +53,8 @@
 			<?php while ( have_posts() ) : the_post(); ?>
 				<div class="item l-carousel__image" style="background-image:url(<?php the_post_thumbnail_url(); ?>)">
 					<a class="l-carousel__hover" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-						<h1 class="l-carousel__title"><?php the_title_attribute(); ?></h1>
 						<h4 class="l-carousel__subtitle"><?php the_time('d-m-Y') ?> por: <?php the_author() ?> </h4>
+						<h1 class="l-carousel__title"><?php the_title_attribute(); ?></h1>
 					</a>
 				</div>
 			<?php endwhile; ?>
