1ae40058f606713e36c0bcfe61f098bce3256e4f
Camila Riveron <camila@isl.co>
2016-08-03 15:59:21 -0400

    carousel

--- wp-content/themes/sela-wpcom/index.php
+++ wp-content/themes/sela-wpcom/index.php
@@ -39,7 +39,7 @@ get_header(); ?>
 	owl.owlCarousel({
         items : 4,
         itemsDesktop : [1500,3], //5 items between 1000px and 901px
-		itemsDesktopSmall : [900,3], // betweem 900px and 601px
+		itemsDesktopSmall : [1100,2], // betweem 900px and 601px
 		itemsTablet: [600,2], //2 items between 600 and 0
 		itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
         navigation : false,
