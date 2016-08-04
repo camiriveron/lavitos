eba260a715ce8e64640291793b4e14f58e6296e6
Camila Riveron <camila@isl.co>
2016-08-04 15:58:15 -0400

    comentarios texto

--- wp-content/themes/sela-wpcom/comments.php
+++ wp-content/themes/sela-wpcom/comments.php
@@ -14,7 +14,7 @@ if ( post_password_required() )
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'sela' ),
+				printf( _nx( 'Un comentario en &ldquo;%2$s&rdquo;', '%1$s comentarios en &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'sela' ),
 					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 			?>
 		</h2>
