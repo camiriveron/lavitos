832cd21facf6faa6112f2136c984fb13f0370282
Camila Riveron <camila@isl.co>
2016-08-05 08:55:19 -0400

    social share code

--- wp-content/themes/sela-wpcom/functions.php
+++ wp-content/themes/sela-wpcom/functions.php
@@ -250,11 +250,9 @@ function crunchify_social_sharing_buttons($content) {
 		$crunchifyThumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' );
  
 		// Construct sharing URL without using any script
-		$twitterURL = 'https://twitter.com/intent/tweet?text='.$crunchifyTitle.'&amp;url='.$crunchifyURL.'&amp;via=Crunchify';
+		$twitterURL = 'https://twitter.com/intent/tweet?text='.$crunchifyTitle.'&amp;url='.$crunchifyURL;
 		$facebookURL = 'https://www.facebook.com/sharer/sharer.php?u='.$crunchifyURL;
 		$googleURL = 'https://plus.google.com/share?url='.$crunchifyURL;
-		$bufferURL = 'https://bufferapp.com/add?url='.$crunchifyURL.'&amp;text='.$crunchifyTitle;
-		$whatsappURL = 'whatsapp://send?text='.$crunchifyTitle . ' ' . $crunchifyURL;
 		$linkedInURL = 'https://www.linkedin.com/shareArticle?mini=true&url='.$crunchifyURL.'&amp;title='.$crunchifyTitle;
  
 		// Based on popular demand added Pinterest too
@@ -265,7 +263,6 @@ function crunchify_social_sharing_buttons($content) {
 		$content .= '<div class="crunchify-social">';
 		$content .= '<h4>- Comparte esta historia -</h4> <a class="crunchify-link crunchify-twitter" href="'. $twitterURL .'" target="_blank">Twitter</a>';
 		$content .= '<a class="crunchify-link crunchify-facebook" href="'.$facebookURL.'" target="_blank">Facebook</a>';
-		$content .= '<a class="crunchify-link crunchify-whatsapp" href="'.$whatsappURL.'" target="_blank">WhatsApp</a>';
 		$content .= '<a class="crunchify-link crunchify-googleplus" href="'.$googleURL.'" target="_blank">Google+</a>';
 		$content .= '<a class="crunchify-link crunchify-linkedin" href="'.$linkedInURL.'" target="_blank">LinkedIn</a>';
 		$content .= '<a class="crunchify-link crunchify-pinterest" href="'.$pinterestURL.'" target="_blank">Pin It</a>';
