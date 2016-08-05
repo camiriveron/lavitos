e8e1b452fb78390f3a85d71c4b634de6529d1af3
Camila Riveron <camila@isl.co>
2016-08-05 11:27:35 -0400

    a espanol algunas cosas

--- wp-content/themes/sela-wpcom/content-image.php
+++ wp-content/themes/sela-wpcom/content-image.php
@@ -25,7 +25,7 @@
 		</div><!-- .entry-meta -->
 
 		<div class="entry-content">
-			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'sela' ) ); ?>
+			<?php the_content( __( 'Seguir leyendo <span class="meta-nav">&rarr;</span>', 'sela' ) ); ?>
 			<?php
 				wp_link_pages( array(
 					'before' => '<div class="page-links">' . __( 'Pages:', 'sela' ),
