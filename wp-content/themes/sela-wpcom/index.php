3b029d674c836af72b72d2af9e2066481a026c9e
Camila Riveron <camila@isl.co>
2016-08-05 16:26:16 -0400

    user data

--- wp-content/themes/sela-wpcom/index.php
+++ wp-content/themes/sela-wpcom/index.php
@@ -27,6 +27,24 @@ get_header(); ?>
 		</div>
 <?php endif; ?>
 
+<div id="page" class="hfeed site">
+<div id="content" class="site-content">
+
+<div id="primary" class="content-area">
+<main id="main" class="site-main" role="main">
+
+<?php if ( have_posts() ) : ?>
+
+<?php else : ?>
+
+<?php get_template_part( 'content', 'none' ); ?>
+
+<?php endif; ?>
+
+</main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
 
 <script>
 (function($) {
