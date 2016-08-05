dcac09233c2b4e3fd0a4e152dc1824f225125c1f
Camila Riveron <camila@isl.co>
2016-08-05 11:50:59 -0400

    traducciones

--- wp-content/themes/sela-wpcom/content-none.php
+++ wp-content/themes/sela-wpcom/content-none.php
@@ -8,7 +8,7 @@
 
 <section class="no-results not-found">
 	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', 'sela' ); ?></h1>
+		<h1 class="page-title"><?php _e( 'Sin resultados', 'sela' ); ?></h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
