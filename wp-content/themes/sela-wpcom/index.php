a3338e2e19c778822f3ab5e4f5aae8e873c3cb71
Camila Riveron <camila@isl.co>
2016-08-08 13:01:12 -0400

    scale image on hover

--- wp-content/themes/sela-wpcom/index.php
+++ wp-content/themes/sela-wpcom/index.php
@@ -15,7 +15,9 @@ get_header(); ?>
 				<?php while ( have_posts() ) : the_post(); ?>
 					<?php if ( has_post_thumbnail() ) : ?>
 					    <div class="item l-carousel__image">
-					    	<?php the_post_thumbnail('sela-carousel-thumbnail'); ?>
+					    	<div class="scale__image__wrapper">
+					    		<?php the_post_thumbnail('sela-carousel-thumbnail'); ?>
+					    	</div> 	
 							<a class="l-carousel__hover" href="<?php the_permalink(); ?>">
 								<h4 class="l-carousel__subtitle"><?php the_time('d-m-Y') ?> por: <?php the_author() ?> </h4>
 								<h1 class="l-carousel__title"><?php the_title_attribute(); ?></h1>
