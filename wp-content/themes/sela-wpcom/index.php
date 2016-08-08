0418af4dbb7edf459d233d754ad0f48c3d9730ae
Camila Riveron <camila@isl.co>
2016-08-08 14:33:24 -0400

    posts homepage

--- wp-content/themes/sela-wpcom/index.php
+++ wp-content/themes/sela-wpcom/index.php
@@ -36,33 +36,28 @@ get_header(); ?>
 
 <?php if ( have_posts() ) : ?>
 
-	<?php
-			// Start the Loop.
-			while ( have_posts() ) : the_post();
+	<?php query_posts('showposts=3'); if (have_posts()) : while (have_posts()) : the_post(); 
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'content', get_post_format() );
 
-			// End the loop.
-			endwhile;
+	get_template_part( 'content', get_post_format() );
 
-			// Previous/next page navigation.
-			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-				'next_text'          => __( 'Next page', 'twentyfifteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
-			) );
-	?>
+	 endwhile;
 
-	<?php else : ?>
+
+	 // Previous/next page navigation.
+	the_posts_pagination( array(
+		'prev_text'          => __( 'Previous page', 'sela' ),
+		'next_text'          => __( 'Next page', 'sela' ),
+		'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'sela' ) . ' </span>',
+	) );
+
+	 ?>
+
+<?php else : ?>
 
 	<?php get_template_part( 'content', 'none' ); ?>
 
-	<?php endif; ?>
+<?php endif; ?>
 
 </main><!-- #main -->
 </div><!-- #primary -->
