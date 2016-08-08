4ae737e6c04eebe95d72c216bf77c93bd572d4d8
Camila Riveron <camila@isl.co>
2016-08-08 10:58:31 -0400

    anterior/siguiente

--- wp-content/themes/sela-wpcom/single.php
+++ wp-content/themes/sela-wpcom/single.php
@@ -28,40 +28,42 @@ get_header(); ?>
 
 				<?php sela_post_nav(); ?>
 
-				<div class="related-posts">
-				<h3 class="widget-title">Related posts</h3>
-				<?php
-				  $orig_post = $post;
-				  global $post;
-				  $tags = wp_get_post_tags($post->ID);
-				   
-				  if ($tags) {
-				  $tag_ids = array();
-				  foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
-				  $args=array(
-				  'tag__in' => $tag_ids,
-				  'post__not_in' => array($post->ID),
-				  'posts_per_page'=>3, // Number of related posts to display.
-				  'caller_get_posts'=>1
-				  );
-				   
-				  $my_query = new wp_query( $args );
-				 
-				  while( $my_query->have_posts() ) {
-				  $my_query->the_post();
-				  ?>
-				   
-				  <div class="relatedthumb">
-				    <a rel="external" href="<? the_permalink()?>"><?php the_post_thumbnail(array(150,150)); ?><br />
-				    <?php the_title(); ?>
-				    </a>
-				  </div>
-				   
-				  <? }
-				  }
-				  $post = $orig_post;
-				  wp_reset_query();
-				  ?>
+				<div class="related-posts__wrapper">
+					<h3 class="widget-title">Related posts</h3>
+					<div class="related-posts">
+					<?php
+					  $orig_post = $post;
+					  global $post;
+					  $tags = wp_get_post_tags($post->ID);
+					   
+					  if ($tags) {
+					  $tag_ids = array();
+					  foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+					  $args=array(
+					  'tag__in' => $tag_ids,
+					  'post__not_in' => array($post->ID),
+					  'posts_per_page'=>3, // Number of related posts to display.
+					  'caller_get_posts'=>1
+					  );
+					   
+					  $my_query = new wp_query( $args );
+					 
+					  while( $my_query->have_posts() ) {
+					  $my_query->the_post();
+					  ?>
+					   
+					  <div class="relatedthumb">
+					    <a rel="external" href="<? the_permalink()?>"><?php the_post_thumbnail(array(150,150)); ?><br />
+					    <?php the_title(); ?>
+					    </a>
+					  </div>
+					   
+					  <? }
+					  }
+					  $post = $orig_post;
+					  wp_reset_query();
+					  ?>
+					</div>
 				</div>
 
 				<?php
