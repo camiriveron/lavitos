85cb177e6e1d8012c4e196a3bda91e809b183939
Camila Riveron <camila@isl.co>
2016-08-08 14:25:44 -0400

    posts homepage

--- wp-content/themes/sela-wpcom/index.php
+++ wp-content/themes/sela-wpcom/index.php
@@ -36,11 +36,33 @@ get_header(); ?>
 
 <?php if ( have_posts() ) : ?>
 
-<?php else : ?>
+	<?php
+			// Start the Loop.
+			while ( have_posts() ) : the_post();
 
-<?php get_template_part( 'content', 'none' ); ?>
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
 
-<?php endif; ?>
+			// End the loop.
+			endwhile;
+
+			// Previous/next page navigation.
+			the_posts_pagination( array(
+				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
+				'next_text'          => __( 'Next page', 'twentyfifteen' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+			) );
+	?>
+
+	<?php else : ?>
+
+	<?php get_template_part( 'content', 'none' ); ?>
+
+	<?php endif; ?>
 
 </main><!-- #main -->
 </div><!-- #primary -->
