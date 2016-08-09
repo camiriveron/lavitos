a0d909c5d54185914bb4e1b39504cbeeb71505cd
Camila Riveron <camila@isl.co>
2016-08-09 15:53:31 -0400

    security issues

--- wp-config.php
+++ wp-config.php
@@ -41,6 +41,9 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+/* Disable File Editing on wp-admin */
+define('DISALLOW_FILE_EDIT', true);
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
