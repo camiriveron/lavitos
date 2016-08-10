caca94cce6204ccf6322bff053bb3674c635a766
Camila Riveron <camila@isl.co>
2016-08-10 11:16:32 -0400

    ajax load more

--- wp-content/themes/sela-wpcom/page-templates/front-page.php
+++ wp-content/themes/sela-wpcom/page-templates/front-page.php
@@ -12,7 +12,11 @@ get_header(); ?>
 	<div id="content" class="site-content">
 
 	<div id="primary" class="content-area front-page-content-area">
-		
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'content', 'page' ); ?>
+
+		<?php endwhile; // end of the loop. ?>
 	</div><!-- #primary -->
 
 	<?php get_sidebar( 'front-page' ); ?>
