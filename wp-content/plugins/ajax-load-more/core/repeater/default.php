18ff20e141b9b72a69be9cfec29c2504d94057b0
Camila Riveron <camila@isl.co>
2016-08-10 10:42:23 -0400

    ajax load more

--- wp-content/plugins/ajax-load-more/core/repeater/default.php
+++ wp-content/plugins/ajax-load-more/core/repeater/default.php
@@ -1,9 +1,8 @@
-<li<?php if (! has_post_thumbnail() ) { echo ' class="no-img"'; } ?>>
-   <?php if ( has_post_thumbnail() ) { the_post_thumbnail(array(150,150));
-   }?>
-   <h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
-   <p class="entry-meta">
-       <?php the_time("F d, Y"); ?>
-   </p>
-   <?php the_excerpt(); ?>
-</li>
+<div class="infinite-scroll__item scale__image__wrapper">
+   <?php the_post_thumbnail("sela-carousel-thumbnail"); ?>
+   <a class="infinite-scroll__hover" href="<?php the_permalink(); ?>">
+      <h1 class="infinite-scroll__title"><?php the_title_attribute(); ?></h1>
+      <h4 class="infinite-scroll__subtitle"><?php the_time("d-m-Y") ?> por: <?php the_author() ?> </h4>
+      <span class="infinite-scroll__resumen"><?php the_excerpt(); ?><span>
+   </a>
+</div>
