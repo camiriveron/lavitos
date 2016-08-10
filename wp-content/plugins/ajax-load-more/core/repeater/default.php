9bbb8580053450c8bd9f05d0e2ecbd5f5212ff62
Camila Riveron <camila@isl.co>
2016-08-10 11:00:13 -0400

    ajax load more

--- wp-content/plugins/ajax-load-more/core/repeater/default.php
+++ wp-content/plugins/ajax-load-more/core/repeater/default.php
@@ -1,5 +1,5 @@
 <div class="infinite-scroll__item scale__image__wrapper">
-   <?php the_post_thumbnail("sela-carousel-thumbnail"); ?>
+   <?php the_post_thumbnail("sela-grid-thumbnail"); ?>
    <a class="infinite-scroll__hover" href="<?php the_permalink(); ?>">
       <h1 class="infinite-scroll__title"><?php the_title_attribute(); ?></h1>
       <h4 class="infinite-scroll__subtitle"><?php the_time("d-m-Y") ?> por: <?php the_author() ?> </h4>
