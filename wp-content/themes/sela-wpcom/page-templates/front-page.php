75053e9d25e442415afbf606fcf79e19dc56bcfa
Camila Riveron <camila@isl.co>
2016-08-10 11:24:49 -0400

    ajax load more

--- wp-content/themes/sela-wpcom/page-templates/front-page.php
+++ wp-content/themes/sela-wpcom/page-templates/front-page.php
@@ -11,18 +11,13 @@ get_header(); ?>
 <div id="page" class="hfeed site">
 	<div id="content" class="site-content">
 
-	<div id="primary" class="content-area front-page-content-area">
-		<?php while ( have_posts() ) : the_post(); ?>
+		<div id="primary" class="content-area front-page-content-area">
+			<?php while ( have_posts() ) : the_post(); ?>
 
-			<?php get_template_part( 'content', 'page' ); ?>
+				<?php get_template_part( 'content', 'page' ); ?>
 
-		<?php endwhile; // end of the loop. ?>
-	</div><!-- #primary -->
+			<?php endwhile; // end of the loop. ?>
+		</div><!-- #primary -->
+	</div><!-- #content -->
 
-</div><!-- #page -->
-
-	<?php get_sidebar( 'front-page' ); ?>
-
-
-<?php get_sidebar( 'footer' ); ?>
 <?php get_footer(); ?>
