f2361c4ef940eda95d7908306c24f58324f1f04f
Camila Riveron <camila@isl.co>
2016-08-11 11:42:09 -0400

    infinite cards title

--- wp-content/plugins/ajax-load-more/core/repeater/default.php
+++ wp-content/plugins/ajax-load-more/core/repeater/default.php
@@ -2,9 +2,9 @@
    <?php the_post_thumbnail("sela-carousel-thumbnail"); ?>
    <a class="infinite-scroll__hover" href="<?php the_permalink(); ?>">
    	  <span class="infinite-scroll__resumen"><?php the_excerpt(); ?></span>
-   	  <div>
-   	  	<h1 class="infinite-scroll__title"><?php the_title_attribute(); ?></h1>
-      	<h4 class="infinite-scroll__subtitle"><?php the_time("d-m-Y") ?> por: <?php the_author() ?> </h4>
+   	  <div class="infinite-scroll__title-area">
+      	  	<h1 class="infinite-scroll__title"><?php the_title_attribute(); ?></h1>
+         	<h4 class="infinite-scroll__subtitle"><?php the_time("d-m-Y") ?> por: <?php the_author() ?> </h4>
    	  </div>
    </a>
    <a class="infinite-scroll__hover--small" href="<?php the_permalink(); ?>">
