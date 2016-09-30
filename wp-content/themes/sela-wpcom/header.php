258a71bcf06dd3bda410c3aaa2d87746b57f3e37
Camila Riveron <camila@isl.co>
2016-09-29 20:57:46 -0400

    Open sans

--- wp-content/themes/sela-wpcom/header.php
+++ wp-content/themes/sela-wpcom/header.php
@@ -15,6 +15,7 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link href='https://fonts.googleapis.com/css?family=Meddon' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700" rel="stylesheet">
 <meta name="google-site-verification" content="U-qpGf9LNGCYOP8qWE_utKmAd_SZJqB3u8isaMraVc0"/>
 <?php wp_head(); ?>
 </head>
