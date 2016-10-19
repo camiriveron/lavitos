f08227e049755ecd5217e93a2c93061702521c33
Camila Riveron <camila@isl.co>
2016-10-19 12:02:48 -0400

    facebook comments title

--- wp-content/themes/sela-wpcom/comments.php
+++ wp-content/themes/sela-wpcom/comments.php
@@ -8,10 +8,9 @@
 if ( post_password_required() )
 	return;
 ?>
-<div class="fb-comments" data-href="<?php the_permalink();?>" data-width="100%" data-numposts="10"></div>
-	<div id="comments" class="comments-area">
-		<h2 class="comments-title">
-			Comentarios
-		</h2>
+<div id="comments" class="comments-area">
+	<h3 class="comments-title">
+		Comentarios
+	</h3>
 	<div class="fb-comments" data-href="<?php the_permalink();?>" data-width="100%" data-numposts="10"></div>
 </div><!-- #comments -->
