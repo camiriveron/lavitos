c28413b5317c1786d9b7ad2ffdc3d612ed3118ea
Camila Riveron <camila@isl.co>
2016-10-24 23:10:19 -0400

    related posts vamo

--- wp-content/themes/sela-wpcom/single.php
+++ wp-content/themes/sela-wpcom/single.php
@@ -34,11 +34,13 @@ get_header(); ?>
 
 				<?php
 				$tags = wp_get_post_terms( get_queried_object_id(), 'post_tag', ['fields' => 'ids'] );
+				$do_not_duplicate = array();
 				$args = [
 					'post_type' => 'post',
 				    'post__not_in'        => array( get_queried_object_id() ),
-				    'posts_per_page'      => 3,
+				    'posts_per_page'      => 10,
 				    'ignore_sticky_posts' => 1,
+				    'post_status' => 'publish',
 				    'tax_query' => [
 				        [
 				            'taxonomy' => 'post_tag',
@@ -49,23 +51,34 @@ get_header(); ?>
 				$my_query = new wp_query( $args );
 
 				if( $my_query->have_posts() ) {
+					$i = 0;
 				    echo '<div class="related-posts__wrapper">
 						<h3 class="widget-title">Historias Relacionadas</h3>
 						<div class="related-posts">';
-				        while( $my_query->have_posts() ) {
-				            $my_query->the_post(); ?>
-				            <div class="relatedthumb">
-							    <a rel="external" href="<? the_permalink()?>">
-								    <div class="scale__image__wrapper">
-								    	<?php the_post_thumbnail(array(200,200)); ?>
-								    </div>
-								    <?php the_title(); ?>
-							    </a>
-						  </div>
+				        while( $my_query->have_posts() || $i < 3 ) {
+				            $my_query->the_post();
+
+				            if ( !in_array( $post->ID, $do_not_duplicate ) ) {
+
+				   			 $do_not_duplicate[] = $post->ID; 
+				   			 $i += 1;
+
+				             ?>
+
+					            <div class="relatedthumb">
+								    <a rel="external" href="<? the_permalink()?>">
+									    <div class="scale__image__wrapper">
+									    	<?php the_post_thumbnail(array(200,200)); ?>
+									    </div>
+									    <?php the_title(); ?>
+								    </a>
+							  </div>
 				        <?php }
+				   		}
 				        wp_reset_postdata();
 				    echo '</div></div><!--related-->';
-				}
+
+				  }
 				?>
 
 				<?php
