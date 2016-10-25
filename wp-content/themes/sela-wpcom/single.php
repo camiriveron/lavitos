2e67a3979379fc9a1e0fa8837477575e5ed0a3a8
Camila Riveron <camila@isl.co>
2016-10-24 22:47:08 -0400

    related posts

--- wp-content/themes/sela-wpcom/single.php
+++ wp-content/themes/sela-wpcom/single.php
@@ -33,54 +33,39 @@ get_header(); ?>
 				?>	
 
 				<?php
-				  $orig_post = $post;
-				  global $post;
-				  $tags = wp_get_post_tags($post->ID);
-				   
-				  if ($tags) {
-				  $tag_ids = array();
-				  foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
-				  $args=array(
-				  'tag__in' => $tag_ids,
-				  'post__not_in' => array($post->ID),
-				  'posts_per_page'=>3, // Number of related posts to display.
-				  'ignore_sticky_posts'=>1
-				  );
-				   
-				  $my_query = new wp_query( $args );
-
-
-				 if( $my_query->have_posts() ){
-				 	?>
-				 	<div class="related-posts__wrapper">
+				$tags = wp_get_post_terms( get_queried_object_id(), 'post_tag', ['fields' => 'ids'] );
+				$args = [
+				    'post__not_in'        => array( get_queried_object_id() ),
+				    'posts_per_page'      => 3,
+				    'ignore_sticky_posts' => 1,
+				    'orderby'             => 'rand',
+				    'tax_query' => [
+				        [
+				            'taxonomy' => 'post_tag',
+				            'terms'    => $tags
+				        ]
+				    ]
+				];
+				$my_query = new wp_query( $args );
+				if( $my_query->have_posts() ) {
+				    echo '<div class="related-posts__wrapper">
 						<h3 class="widget-title">Historias Relacionadas</h3>
-						<div class="related-posts">
-				  <?php		
-				  while( $my_query->have_posts() ) { 
-				  	$my_query->the_post();
-				  ?>	
-
-						  <div class="relatedthumb">
-						    <a rel="external" href="<? the_permalink()?>">
-							    <div class="scale__image__wrapper">
-							    	<?php the_post_thumbnail(array(200,200)); ?>
-							    </div>
-							    <?php the_title(); ?>
-						    </a>
+						<div class="related-posts">';
+				        while( $my_query->have_posts() ) {
+				            $my_query->the_post(); ?>
+				            <div class="relatedthumb">
+							    <a rel="external" href="<? the_permalink()?>">
+								    <div class="scale__image__wrapper">
+								    	<?php the_post_thumbnail(array(200,200)); ?>
+								    </div>
+								    <?php the_title(); ?>
+							    </a>
 						  </div>
-					   
-					  <?	 } //while
-					  ?>
-				 	
-				 			</div>
-				 		</div>
-				  	<?php
-							} //if have posts
-					  } //if tags
-					  $post = $orig_post;
-					  wp_reset_query();
-					  ?>
-					
+				        <?php }
+				        wp_reset_postdata();
+				    echo '</div></div><!--related-->';
+				}
+				?>
 
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
